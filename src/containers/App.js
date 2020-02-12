e7da711db5964932b8ba12c15dc0ae0b9318515f
Алексей Бельченков <u608110@gmail.com>
2020-02-12 17:51:05 +0400

    Creating HOC

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,12 +21,19 @@ const AppWrapper = styled.div`
 
 class App extends Component {
   render() {
-    return (
+      const lanes = [
+          { id: 1, title: 'To Do' },
+          { id: 2, title: 'In Progress' },
+          { id: 3, title: 'Review' },
+          { id: 4, title: 'Done' },
+      ];
+
+      return (
       <>
         <GlobalStyle />
         <AppWrapper>
           <Header />
-          <Board />
+          <Board lanes={lanes} dataSource={'../../assets/data.json'} />
         </AppWrapper>
       </>
     );
--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+
 import Lane from '../components/Lane/Lane';
+import withDataFetching from '../withDataFetching';
 
 const BoardWrapper = styled.div`
   display: flex;
@@ -13,59 +15,18 @@ const BoardWrapper = styled.div`
   }
 `;
 
-class Board extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: [],
-      loading: true,
-      error: ''
-    };
-  }
-  
-  async componentDidMount() {
-    try {
-      const tickets = await fetch('../../assets/data.json');
-      const ticketsJSON = await tickets.json();
+const Board = ({ lanes, loading, error, data }) => (
+    <BoardWrapper>
+      {lanes.map(lane => (
+        <Lane
+            key={lane.id}
+            title={lane.title}
+            loading={loading}
+            error={error}
+            tickets={data.filter(ticket => ticket.lane === lane.id)}
+        />
+      ))}
+    </BoardWrapper>
+);
 
-      if (ticketsJSON) {
-        this.setState({
-          data: ticketsJSON,
-          loading: false
-        })
-      }
-
-    } catch (error) {
-      this.setState({
-        data: error.message,
-        loading: false
-      })
-    }
-  }
-
-  render() {
-    const { data, loading, error } = this.state;
-
-    const lanes = [
-      { id: 1, title: 'To Do' },
-      { id: 2, title: 'In Progress' },
-      { id: 3, title: 'Review' },
-      { id: 4, title: 'Done' },
-    ];
-
-    return (
-      <BoardWrapper>
-        {lanes.map(lane => (
-          <Lane
-              key={lane.id}
-              title={lane.title}
-              error={error}
-              tickets={data.filter(ticket => ticket.lane === lane.id)}
-          />
-        ))}
-      </BoardWrapper>
-    );
-  }
-}
-
-export default Board;
+export default withDataFetching(Board);
